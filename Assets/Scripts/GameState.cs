8e1cf42d100e3af1f9ff15c8b2028b8e9fc18858
Jordan Bowering <jordanbowering7@gmail.com>
2021-04-27 22:52:55 +0100

    Implemted Health UI which now updates on enemy reaching end.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public int health;
+    public int money;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
